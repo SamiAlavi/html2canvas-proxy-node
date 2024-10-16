e553fb1f538913ca3999f8c92e03c749bc00b891
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2024-10-16 02:05:37 +0200

    start server

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const express = require('express');
 
 const app = express();
 app.use('/', proxy());
+
+// Set the port from an environment constiable, or default to 8080
+const port = process.env.PORT || 8080;
+
+// Start the server
+app.listen(port, function() {
+    console.log(`Server is running on port ${port}`);
+});
